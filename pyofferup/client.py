ed8903029bcacdc5b912dee6791a2f66bd57165e
Daven Desai <daven.desai@outlook.com>
2023-08-01 23:43:01 -0700

    Early POC of GraphQL HTTP API

--- pyofferup/client.py
+++ pyofferup/client.py
@@ -1,32 +1,35 @@
 import requests
+import json
+from pathlib import Path
 
 endpoint = "http://offerup.com/api/graphql"
 
-query = """{
-  __schema {
-    types {
-      name
-    }
-  }
-}
-"""
+# Seattle, WA
+search_params = [
+  { 'key': 'platform', 'value': 'web' },
+  { 'key': 'lon', 'value': '-122.2475' },
+  { 'key': 'lat', 'value': '47.7541' },
+  { 'key': 'limit', 'value': str(50) },
+]
 
-params = {
-  'query': query
-}
-
-def __post_graphql_request__(query: str):
+def __get_graphql_request__(query: str):
   s = requests.Session()
   response = s.get(endpoint)
   cookies = dict(response.cookies)
-     
+  
   return s.get(endpoint,
-               params=params,
+               params={
+                 'query': query,
+                 'variables': json.dumps({
+                   'searchParams': search_params
+                 })
+               },
                headers={"Content-Type": "application/json",
                         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"},
                cookies=cookies,
-               timeout=0.2)
+               timeout=0.5)
 
-
-gql_response = __post_graphql_request__(query)
+query = Path('./pyofferup/queries/GetModularFeed.gql').read_text()
+gql_response = __get_graphql_request__(query)
+print(gql_response.status_code)
 print(gql_response.content)
